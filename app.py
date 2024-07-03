812475594bff10e632a9f894cf4217308c5ce231
Rotimi Olakunle O. Awomodu <83477902+Steven-Awo@users.noreply.github.com>
2024-07-03 16:33:58 +0100

    more recommendations

--- app.py
+++ app.py
@@ -52,6 +52,12 @@ if  st.button('Showing the Recommendation'):
     name_of_movie_recommendation,movie_recommended_poster = recomMovie(the_selected_movies)
 
     coln1, coln2, coln3, coln4, coln5 = st.columns(5)
+    coln6, coln7, coln8, coln9, coln10 = st.columns(5)
+    coln11, coln12, coln13, coln14, coln15 = st.columns(5)
+    coln16, coln17, coln18, coln19, coln20 = st.columns(5)
+    coln21, coln22, coln23, coln24, coln25 = st.columns(5)
+    coln26, coln27, coln28, coln29, coln30 = st.columns(5)
+
     with coln1:
         st.text(name_of_movie_recommendation[0])
         st.image(movie_recommended_poster[0])
@@ -68,3 +74,79 @@ if  st.button('Showing the Recommendation'):
     with coln5:
         st.text(name_of_movie_recommendation[4])
         st.image(movie_recommended_poster[4])
+    with coln6:
+        st.text(name_of_movie_recommendation[5])
+        st.image(movie_recommended_poster[5])
+    with coln7:
+        st.text(name_of_movie_recommendation[6])
+        st.image(movie_recommended_poster[6])
+    with coln8:
+        st.text(name_of_movie_recommendation[7])
+        st.image(movie_recommended_poster[7])
+    with coln9:
+        st.text(name_of_movie_recommendation[8])
+        st.image(movie_recommended_poster[8])
+    with coln10:
+        st.text(name_of_movie_recommendation[9])
+        st.image(movie_recommended_poster[9])
+    with coln11:
+        st.text(name_of_movie_recommendation[10])
+        st.image(movie_recommended_poster[10])
+    with coln12:
+        st.text(name_of_movie_recommendation[11])
+        st.image(movie_recommended_poster[11])
+    with coln13:
+        st.text(name_of_movie_recommendation[12])
+        st.image(movie_recommended_poster[12])
+    with coln14:
+        st.text(name_of_movie_recommendation[13])
+        st.image(movie_recommended_poster[13])
+    with coln15:
+        st.text(name_of_movie_recommendation[14])
+        st.image(movie_recommended_poster[14])
+    with coln16:
+        st.text(name_of_movie_recommendation[15])
+        st.image(movie_recommended_poster[15])
+    with coln17:
+        st.text(name_of_movie_recommendation[16])
+        st.image(movie_recommended_poster[16])
+    with coln18:
+        st.text(name_of_movie_recommendation[17])
+        st.image(movie_recommended_poster[17])
+    with coln19:
+        st.text(name_of_movie_recommendation[18])
+        st.image(movie_recommended_poster[18])
+    with coln20:
+        st.text(name_of_movie_recommendation[19])
+        st.image(movie_recommended_poster[19])
+    with coln21:
+        st.text(name_of_movie_recommendation[20])
+        st.image(movie_recommended_poster[20])
+    with coln22:
+        st.text(name_of_movie_recommendation[21])
+        st.image(movie_recommended_poster[21])
+    with coln23:
+        st.text(name_of_movie_recommendation[22])
+        st.image(movie_recommended_poster[22])
+    with coln24:
+        st.text(name_of_movie_recommendation[23])
+        st.image(movie_recommended_poster[23])
+    with coln25:
+        st.text(name_of_movie_recommendation[24])
+        st.image(movie_recommended_poster[24])
+    with coln26:
+        st.text(name_of_movie_recommendation[25])
+        st.image(movie_recommended_poster[25])
+    with coln27:
+        st.text(name_of_movie_recommendation[26])
+        st.image(movie_recommended_poster[26])
+    with coln28:
+        st.text(name_of_movie_recommendation[27])
+        st.image(movie_recommended_poster[27])
+    with coln29:
+        st.text(name_of_movie_recommendation[28])
+        st.image(movie_recommended_poster[28])
+    with coln30:
+        st.text(name_of_movie_recommendation[29])
+        st.image(movie_recommended_poster[29])
+    
